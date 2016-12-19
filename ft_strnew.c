a996393cf77cb1ff0757e028435ceb2aa52ddca8
Florian DIKILU <fdikilu@e1r2p21.42.fr>
2016-12-19 17:21:19 +0100

    My libft

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: fdikilu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 20:11:28 by fdikilu           #+#    #+#             */
-/*   Updated: 2016/12/02 18:42:26 by fdikilu          ###   ########.fr       */
+/*   Updated: 2016/12/19 17:19:04 by fdikilu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	*ft_strnew(size_t size)
 
 	i = 0;
 	str = NULL;
-	str = (char *)malloc(sizeof(*str) * (size + 1));
-	if (str != NULL)
-		ft_bzero((void *)str, size + 1);
+	if(!(str = (char *)malloc(sizeof(*str) * (size + 1))))
+		return (NULL);
+	ft_bzero((void *)str, size + 1);
 	return (str);
 }
