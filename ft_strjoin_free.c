70d26b6221465c5125e0b45de27fc84d52b4393f
Florian DIKILU <fdikilu@e1r11p21.42.fr>
2017-11-28 20:05:50 +0100

    My libft

--- ft_strjoin_free.c
+++ ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: fdikilu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/07 14:02:35 by fdikilu           #+#    #+#             */
-/*   Updated: 2017/04/07 14:02:42 by fdikilu          ###   ########.fr       */
+/*   Updated: 2017/11/28 20:04:53 by fdikilu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ char	*ft_strjoin_free(char const *s1, char const *s2)
 {
 	int		i;
 	int		j;
-	char	*tmp;
 	char	*str;
 
 	if (!(s1 && s2))
 		return (NULL);
 	i = 0;
 	j = 0;
-	tmp = (char *)s1;
-	free((void *)s1);
 	if (!(str = (char *)malloc(sizeof(*str)
 		* (ft_strlen(tmp) + ft_strlen((char *)s2) + 1))))
 		return (NULL);
@@ -37,5 +34,6 @@ char	*ft_strjoin_free(char const *s1, char const *s2)
 		j++;
 	}
 	str[i] = '\0';
+	ft_strdel((char **)&s1);
 	return (str);
 }
