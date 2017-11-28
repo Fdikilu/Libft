afff33593b2d056afe94c554496af292c3b4e659
Florian DIKILU <fdikilu@e1r11p21.42.fr>
2017-11-28 20:01:29 +0100

    My libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: fdikilu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 01:07:45 by fdikilu           #+#    #+#             */
-/*   Updated: 2016/11/29 23:13:22 by fdikilu          ###   ########.fr       */
+/*   Updated: 2017/11/28 19:59:32 by fdikilu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,13 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	t_list	*lst;
 
 	lst = NULL;
-	lst = (t_list *)malloc(sizeof(*lst));
-	if (lst == NULL)
+	if (!(lst = (t_list *)malloc(sizeof(*lst))))
 		return (lst);
 	if (content == NULL)
-	{
-		lst->content = NULL;
-		lst->content_size = 0;
-		lst->next = NULL;
-	}
+		lst = &((t_list){NULL, 0, NULL});
 	else
 	{
-		lst->content = (void *)malloc(content_size);
-		if (lst->content == NULL)
+		if (!(lst->content = (void *)malloc(content_size)))
 			return (NULL);
 		ft_memcpy(lst->content, content, content_size);
 		lst->content_size = content_size;
